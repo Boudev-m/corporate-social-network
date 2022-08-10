7da3010e8462135b6530b622fcf865298dfeb19d
bouimust <moussovic@gmail.com>
2022-08-10 11:24:07 +0200

    traitement d'authentification user

---
+++ back/middleware/auth.js
@@ -0,0 +1,17 @@
+// Gestionnaire de Token
+const jwt = require('jsonwebtoken');
+
+// Vérifie l'authentification utilisateur, pour lui donner l'accès d'afficher, créer, modifier ou supprimer des sauces
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, process.env.PRIVATE_KEY);
+        const userId = decodedToken.userId;
+        req.auth = {
+            userId: userId
+        };
+        next();
+    } catch (error) {
+        res.status(401).json({ error });
+    }
+};
