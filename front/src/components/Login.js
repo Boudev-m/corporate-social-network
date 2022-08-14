83de389f9b967442ba553c566da089a34d30d6c3
bouimust <moussovic@gmail.com>
2022-08-14 09:11:55 +0200

    Ajout vérification du formulaire et requête POST

--- front/src/components/Login.js
+++ front/src/components/Login.js
@@ -1,6 +1,76 @@
 import React from 'react';
+import axios from 'axios';
+
 
 const Login = () => {
+
+    // Véfification du formulaire
+    function CheckForm(e) {
+
+        e.preventDefault(); //annule le comportement par défaut du formulaire et aussi les verificateurs html comme required ou format email)
+        // Les attributs type et required apportent une vérification supplémentaire
+
+        // récupère tous les inputs
+        let input = document.querySelectorAll('.form-group input');
+        let inputError = document.querySelector('.input-error');
+        // De base, le formulaire est valide à chaque soumission du formulaire par l'utilisateur
+        let formIsValid = true;
+        // si un des champs est vide, renvoie une erreur
+        for (let i = 0; i < input.length; i++) {
+            if (!input[i].value) {
+                inputError.textContent = 'Email ou mot de passe invalide';
+                formIsValid = false;
+            }
+        }
+
+        // si le champ email n'est pas conforme
+        if (!/^[\w-]{1,20}@[\w-]{1,20}[.][\w-]{1,5}$/.test(input[0].value)) {
+            inputError.textContent = 'Email ou mot de passe invalide';
+            formIsValid = false;
+        }
+
+        // si le champ password n'est pas conforme
+        if (!/^[a-zA-Z0-9]{8,20}$/.test(input[1].value)) {
+            inputError.textContent = 'Email ou mot de passe invalide';
+            formIsValid = false;
+        }
+
+        // Si le formulaire est conforme
+        if (formIsValid) {
+            console.log('Le formulaire est conforme.');
+            SendForm(e);
+        }
+    };
+
+    // Envoie le formulaire
+    function SendForm(e) {
+        // si un champ vide, renvoyer une erreur
+        e.preventDefault();         // enleve le comportement du bouton 'submit' dans les formulaires
+        const user = {
+            email: document.getElementById('email').value,
+            password: document.getElementById('password').value
+        }
+        const headers = {           // ajout de headers
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+        // Requête POST : authentification de l'user
+        axios.post("http://localhost:3000/api/auth/login", user, { headers })
+            .then((res) => console.log(res.data.token))
+            .then((res) => headers.set('Authorization', 'Bearer ' + res.data.token))
+            // token
+            // .then(document.location.href = "./")     // retour page d'accueil après envoie d'un post
+            .catch((error) => {
+                if (error.response.status === 401) {
+                    document.querySelector('.input-error').textContent = 'Email ou mot de passe invalide';
+                }
+            });
+    }
+
+    function RemoveTextError(e) {
+        document.querySelector('.input-error').textContent = '';
+    }
+
     return (
         <main>
             <div className='main_content center'>
@@ -8,16 +78,18 @@ const Login = () => {
                     <div className="form-group">
                         <label htmlFor="email">Email</label>
                         <br />
-                        <input type="email" name="email" id="email" />
+                        <input type="email" name="email" id="email" onInput={RemoveTextError} required />
                     </div>
                     <br />
-                    <div class="form-group">
+                    <div className="form-group">
                         <label htmlFor="password">Mot de passe</label>
                         <br />
-                        <input type="password" name="password" id="password" />
+                        <input type="password" name="password" id="password" onInput={RemoveTextError} required />
                     </div>
                     <br />
-                    <button>Se connecter</button>
+                    <p class="input-error"></p>
+                    <br />
+                    <button onClick={CheckForm}>Se connecter</button>
                 </form>
             </div >
         </main >
--- front/src/components/Signup.js
+++ front/src/components/Signup.js
@@ -1,6 +1,81 @@
 import React from 'react';
+import axios from 'axios';
 
 const Signup = () => {
+
+    // Véfification du formulaire
+    function CheckForm(e) {
+
+        e.preventDefault(); //annule le comportement par défaut du formulaire et aussi les verificateurs html comme required ou format email)
+        // Les attributs type et required apportent une vérification supplémentaire
+
+        // récupère tous les inputs
+        let input = document.querySelectorAll('.form-group input');
+        // De base, le formulaire est valide à chaque soumission du formulaire par l'utilisateur
+        let formIsValid = true;
+        // si un des champs est vide, renvoie une erreur
+        for (let i = 0; i < input.length; i++) {
+            if (!input[i].value) {
+                input[i].nextSibling.textContent = 'Veuillez entrer une valeur correcte'
+                formIsValid = false;
+            }
+        }
+
+        // si les champs firstName et lastName ne sont pas conformes
+        for (let i = 0; i < 2; i++) {
+            if (!/^[a-zA-Z]{2,20}/.test(input[i].value)) {
+                input[i].nextSibling.textContent = 'Veuillez entrer une valeur correcte'
+                formIsValid = false;
+            }
+        }
+
+        // si le champ email n'est pas conforme
+        if (!/^[\w-]{1,20}@[\w-]{1,20}[.][\w-]{1,5}$/.test(input[2].value)) {
+            input[2].nextSibling.textContent = 'Veuillez entrer une adresse mail correcte';
+            formIsValid = false;
+        }
+
+        // si le champ password n'est pas conforme
+        if (!/^[a-zA-Z0-9]{8,20}$/.test(input[3].value)) {
+            input[3].nextSibling.textContent = 'Veuillez entrer un mot de passe correct (8 caractères min. et sans caractères spéciaux)'
+            formIsValid = false;
+        }
+
+        // Si le formulaire est conforme
+        if (formIsValid) {
+            console.log('Le formulaire est conforme.');
+            SendForm(e);
+        }
+
+    };
+
+    // Envoi du formulaire
+    function SendForm(e) {
+
+        // e.preventDefault();         // enleve le comportement du bouton 'submit' dans les formulaires
+        const user = {
+            lastName: document.getElementById('lastName').value,
+            firstName: document.getElementById('firstName').value,
+            email: document.getElementById('email').value,
+            password: document.getElementById('password').value
+        }
+        const headers = {           // ajout de headers
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+        // Requête POST : inscription de l'user
+        axios.post("http://localhost:3000/api/auth/signup", user, { headers })
+            .then((res) => console.log(res))
+            // puis authentification/login
+            // puis intégrer token dans les headers de requêtes
+            // puis renvoie à la page d'accueil
+            .catch((error) => console.log(error));
+    }
+
+    function RemoveTextError(e) {
+        e.target.nextElementSibling.textContent = '';
+    }
+
     return (
         <main>
             <div className='main_content center'>
@@ -8,28 +83,32 @@ const Signup = () => {
                     <div className="form-group">
                         <label htmlFor="lastName">Nom</label>
                         <br />
-                        <input type="text" name="lastName" id="lastName" />
+                        <input type="text" name="lastName" id="lastName" onInput={RemoveTextError} required />
+                        <p className="input-error"></p>
                     </div>
                     <br />
                     <div className="form-group">
                         <label htmlFor="firstName">Prénom</label>
                         <br />
-                        <input type="text" name="firstName" id="firstName" />
+                        <input type="text" name="firstName" id="firstName" onInput={RemoveTextError} required />
+                        <p className="input-error"></p>
                     </div>
                     <br />
                     <div className="form-group">
                         <label htmlFor="email">Email</label>
                         <br />
-                        <input type="email" name="email" id="email" />
+                        <input type="email" name="email" id="email" onInput={RemoveTextError} required />
+                        <p className="input-error"></p>
                     </div>
                     <br />
                     <div className="form-group">
                         <label htmlFor="password">Mot de passe</label>
                         <br />
-                        <input type="password" name="password" id="password" />
+                        <input type="password" name="password" id="password" onInput={RemoveTextError} required />
+                        <p className="input-error"></p>
                     </div>
                     <br />
-                    <button>Se connecter</button>
+                    <button onClick={CheckForm}>S'inscrire</button>
                 </form>
             </div >
         </main >
