788b90ef93177e65dfcb8c69f865b0944dfd2886
bouimust <moussovic@gmail.com>
2022-08-19 23:21:46 +0200

    configuration de l'authentification

--- back/middleware/auth.js
+++ back/middleware/auth.js
@@ -3,15 +3,22 @@ const jwt = require('jsonwebtoken');
 
 // Vérifie l'authentification utilisateur, pour lui donner l'accès d'afficher, créer, modifier ou supprimer des sauces
 module.exports = (req, res, next) => {
+    console.log('-------------- AUTH --------------');
+    console.log('Vérification du token...');
     try {
         const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, process.env.PRIVATE_KEY);
+        const decodedToken = jwt.verify(token, 'process.env.PRIVATE_KEY');      // à placer dans une var env
         const userId = decodedToken.userId;
         req.auth = {
             userId: userId
         };
+        console.log('Token dans la requête :');
+        console.log(req.headers.authorization.split('Bearer ')[1]);
+        console.log('userId du token:');
+        console.log(req.auth.userId);
         next();
     } catch (error) {
+        console.log('Token non valide.');
         res.status(401).json({ error });
     }
 };
