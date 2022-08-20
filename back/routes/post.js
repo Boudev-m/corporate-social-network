061c7632dc7e7d975abe5d4704b0cddf7c467e0a
bouimust <moussovic@gmail.com>
2022-08-20 09:34:33 +0200

    Ajout des middlewares auth et multer

--- back/routes/post.js
+++ back/routes/post.js
@@ -11,7 +11,7 @@ const multer = require('../middleware/multer-config');
 
 // Routes
 router.get('/', auth, postCtrl.getAllPost);
-router.post('/', /*auth,*/ multer, postCtrl.createPost);
+router.post('/', auth, multer, postCtrl.createPost);
 router.put('/:id', auth, multer, postCtrl.updatePost);
 router.post('/:id/like', auth, postCtrl.likePost);
 router.delete('/:id', auth, postCtrl.deletePost);
