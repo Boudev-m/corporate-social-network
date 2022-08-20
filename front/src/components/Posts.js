7551186d8ac9bf062edadc7d2f50656590744f7f
bouimust <moussovic@gmail.com>
2022-08-20 16:35:39 +0200

    modification de la requête GET posts

--- front/src/components/Posts.js
+++ front/src/components/Posts.js
@@ -6,33 +6,41 @@ const Posts = () => {
     // Requête GET  : affiche la collection des posts
     const [data, setData] = useState([]);
     useEffect(() => {
-        axios
-            .get("http://localhost:3000/api/posts", axios.defaults.headers.common.Authorization)
-            .then((res) => setData(res.data))
-            .catch((error) => console.log(error));
+        const headers = {
+            'Authorization': `Bearer ${localStorage.jwt}`
+        }
+        axios.get("http://localhost:3000/api/posts", { headers })
+            .then((res) => {
+                setData(res.data);
+            })
+            .catch(() => {
+                window.location = '/login';
+            })
     }, []) // le [] est pour une callback
 
     // Requête POST : ajouter/enlever un like
+    // Requête PUT : modifier un post
+    // Requête DELETE : supprimer un post
 
     // si user non authentifié, afficher 'connectez vous pour voir les messages'
     return (
         <main>
             {data
                 .sort((a, b) => b.date - a.date) // trie par date
-                .map((post, index) => (          // parcourt chaque elt du tableau
-                    <div className='main_content' key={index}>
+                .map((post) => (          // parcourt chaque elt du tableau
+                    <div className='main_content' key={post._id}>
                         <div className='name_date_post'>
-                            <p>{post.firstName + ' ' + post.lastName}</p>
-                            <p>Date : {post.date}</p>
+                            <p>{post.userId + ' ' + post.userId}</p>
+                            <p>Posté le {post.date[0]} à {post.date[1]}</p>
                         </div>
                         <div className='message_post'>
-                            <p className='text_post'>{post.message}</p>
+                            <p className='text_post'>{post.text}</p>
                             <div className='image_post'>
-                                <img src="./logo192.png" alt="[Illustration du message]" />
+                                <img src={post.imageUrl} alt="[Illustration du message]" />
                             </div>
                         </div>
                         <div className='like_post'>
-                            <div>{post.like} like{post.like > 1 ? 's' : ''} </div>
+                            <div>{post.likes} like{post.likes > 1 ? 's' : ''} </div>
                             <div></div>
                         </div>
                     </div>
