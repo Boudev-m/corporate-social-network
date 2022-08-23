81be2c1b778a965e9ad1a4f73ed7b5f2aa2ff23c
bouimust <moussovic@gmail.com>
2022-08-23 16:42:57 +0200

    page de modification d'un post

---
+++ front/src/pages/Updatepost.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header';
+import UpdatePost from '../components/UpdatePost';
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            <UpdatePost />
+        </div>
+    );
+};
+
+export default Home;
